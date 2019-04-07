dd7e7f803c4a7f87d97fa5f93816991e76ee9621
ssrome <ssamuelrome@gmail.com>
2019-04-07 16:27:12 +0100

    Updated JS for game logic

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 var playerOne;
-var playerTwo = Math.random();
+var playerTwo;
 var result;
-const rock = document.getElementById('rock');
 
-rock.addEventListener('click', playerOne);
-
-let random = () => {
-    if (playerTwo >= 0 && playerTwo <= 0.33) {
-        return playerTwo = "rock";
+let setPlayerOne = (val) => {
+    playerOne = val;
+    let setPlayerTwo = () => {
+        playerTwo = Math.random();
+        if (playerTwo >= 0 && playerTwo <= 0.33) {
+            return playerTwo = "rock";
+        }
+        else if (playerTwo > 0.33 && playerTwo <= 0.66){
+            return playerTwo = "paper";
+        }
+        else {
+            return playerTwo = "scissors";
+        }  
     }
-    else if (playerTwo > 0.33 && playerTwo <= 0.66){
-        return playerTwo = "paper";
+    setPlayerTwo();
+    let game = () => {
+        if (playerOne === "rock" && playerTwo === "scissors" || playerOne === "paper" && playerTwo === "rock" || playerOne === "scissors" && playerTwo === "paper"){
+          return result = document.getElementById('result').innerHTML = "You picked " + playerOne + " and the computer picked " + playerTwo + ". You win!";
+        }
+        else if (playerOne === playerTwo){
+          return result = document.getElementById('result').innerHTML =  "You picked " + playerOne + " and the computer picked " + playerTwo + ". It's a tie!";
+        }
+        else if (playerOne == null) {
+          return result = document.getElementById('result').innerHTML = "You didn't pick";
+        }
+        else {
+            return result = document.getElementById('result').innerHTML = "You picked " + playerOne + " and the computer picked " + playerTwo + ". You lose!";
+        }
     }
-    else {
-        return playerTwo = "scissors";
-    }
+    game();
 }
-random();
 
-let game = () => {
-    if (playerOne === "rock" && playerTwo === "scissors" || playerOne === "paper" && playerTwo === "rock" || playerOne === "scissors" && playerTwo === "paper"){
-      return result = document.getElementById('result').innerHTML = "The computer picked " + playerTwo + ". You win!";
-    }
-    else if (playerOne === playerTwo){
-      return result = document.getElementById('result').innerHTML =  "The computer picked " + playerTwo + ". It's a tie!";
-    }
-    else if (playerOne == null) {
-      return result = document.getElementById('result').innerHTML = "You didn't pick";
-    }
-    else {
-        return result = document.getElementById('result').innerHTML = "The computer picked " + playerTwo + ". You lose!";
-    }
+let reset = () => {
+    return result = document.getElementById('result').innerHTML = "";
 }
-game();
 
 let date = new Date(Date.now());
 let year = date.getFullYear();

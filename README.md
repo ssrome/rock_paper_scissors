9cb87c6e1b5c382d3a6761b9a8808a128499fb53
ssrome <ssamuelrome@gmail.com>
2021-02-15 22:52:39 +0000

    Updated js and css

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Took a Codewars JavaScript challenge and expanded it to include HTML and CSS.
 
+Used Font Awesome for the icons
--- js/script.js
+++ js/script.js
@@ -1,61 +1,51 @@
-var playerOne;
-var playerTwo;
-var result;
-var rock = '<i class="fas fa-hand-rock"></i>';
-var paper = '<i class="fas fa-hand-paper"></i>';
-var scissors = '<i class="fas fa-hand-scissors"></i>';
+let result = document.querySelector("#result");
+let playerTwo;
 
-
-let setPlayerOne = (val) => {
-    if (val === "rock"){
-         playerOne = rock;
-    }
-    else if (val === "paper"){
-         playerOne = paper;
-    }
-    else if (val === "scissors") {
-         playerOne = scissors;
-    }
-
-    let setPlayerTwo = () => {
-        playerTwo = Math.random();
-        if (playerTwo >= 0 && playerTwo <= 0.33) {
-            return playerTwo = rock;
-        }
-        else if (playerTwo > 0.33 && playerTwo <= 0.66){
-            return playerTwo = paper;
-        }
-        else {
-            return playerTwo = scissors;
-        }  
-    }
-    setPlayerTwo();
-    let game = () => {
-        var picked = "You picked " + playerOne + " and the computer picked " + playerTwo;
-        if (playerOne === rock && playerTwo === scissors || playerOne === paper && playerTwo === rock || playerOne === scissors && playerTwo === paper){
-          return result = document.getElementById('result').innerHTML = picked + "<br/>You win!";
-        }
-        else if (playerOne === playerTwo){
-          return result = document.getElementById('result').innerHTML =  picked + "<br/>It's a tie!";
-        }
-        else if (playerOne == null || playerOne == undefined) {
-          return result = document.getElementById('result').innerHTML = "You didn't pick";
-        }
-        else {
-            return result = document.getElementById('result').innerHTML = picked + "<br/>You lose!";
-        }
-    }
-    game();
+function setPlayerTwo() {
+  playerTwo = Math.random();
+  if (playerTwo >= 0 && playerTwo <= 0.33) {
+    return (playerTwo = "rock");
+  } else if (playerTwo > 0.33 && playerTwo <= 0.66) {
+    return (playerTwo = "paper");
+  } else {
+    return (playerTwo = "scissors");
+  }
 }
 
-let reset = () => {
-    return result = document.getElementById('result').innerHTML = "";
+function resetGame() {
+  result.innerHTML = "";
 }
+
+function playGame(event) {
+  event.preventDefault();
+  let playerOne = event.target.id;
+  playerTwo = setPlayerTwo();
+  if (
+    (playerOne === "rock" && playerTwo === "scissors") ||
+    (playerOne === "paper" && playerTwo === "rock") ||
+    (playerOne === "scissors" && playerTwo === "paper")
+  ) {
+    result.innerHTML = `You picked ${playerOne} and the computer picked ${playerTwo}. You win!`;
+  } else if (playerOne === playerTwo) {
+    result.innerHTML = `You picked ${playerOne} and the computer picked ${playerTwo}. It was a draw!`;
+  } else {
+    result.innerHTML = `You picked ${playerOne} and the computer picked ${playerTwo}. You lose!`;
+  }
+}
+
+let reset = document.querySelector("#reset");
+reset.addEventListener("click", resetGame);
+
+let rock = document.querySelector("#rock");
+rock.addEventListener("click", playGame);
+
+let paper = document.querySelector("#paper");
+paper.addEventListener("click", playGame);
+
+let scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", playGame);
 
 let date = new Date(Date.now());
 let year = date.getFullYear();
-
-function getDate () {
-    let copyright = document.getElementById('copyright').innerHTML = year + " Copyright | All Rights Reserved. Designed by <a href='http://www.saromsa.com' target='_blank'>SaRomSa";
-}; 
-getDate();
+let footer = document.querySelector("#footer");
+footer.innerHTML = `2020 - ${year} Copyright | All Rights Reserved. Designed by <a href='http://www.saromsa.com' target='_blank'>SaRomSa`;
